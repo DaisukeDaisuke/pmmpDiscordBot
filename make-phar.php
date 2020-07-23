783061117a761a1f5c9a71039af671ee6c6d2669
DaisukeDaisuke <akblcmakblcm@gmail.com>
2020-07-23 17:15:01 +0900

    Update make-phar.php

--- make-phar.php
+++ make-phar.php
@@ -21,6 +21,7 @@ $phar = new Phar($file_phar, 0);
 $phar->startBuffering();
 $phar->setSignatureAlgorithm(\Phar::SHA1);
 $phar->buildFromIterator(new \ArrayIterator($files));
+$phar->setStub('<?php __HALT_COMPILER();');
 if(isset($argv[1])&&$argv[1] === "enableCompressAll"){
 	$phar->compressFiles(Phar::GZ);
 }
