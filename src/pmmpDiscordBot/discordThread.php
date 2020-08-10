2504b18e61ad3329b5c5c66c372128e9aa340b15
DaisukeDaisuke <akblcmakblcm@gmail.com>
2020-08-10 19:25:29 +0900

    merge https://gist.github.com/WhiteGrouse/061c9e50b3096aec911548188bca09df Thank you very much!

--- src/pmmpDiscordBot/discordThread.php
+++ src/pmmpDiscordBot/discordThread.php
@@ -1,12 +1,11 @@
 <?php
+
 namespace pmmpDiscordBot;
 
-use pocketmine\Server;
 use pocketmine\utils\TextFormat;
-use pocketmine\command\ConsoleCommandSender;
 
 class discordThread extends \Thread{
-    public $file;
+	public $file;
 
 	public $test = "";
 	public $stopped = false;
@@ -20,17 +19,23 @@ class discordThread extends \Thread{
 	public $send_channelId;
 	public $receive_channelId;
 
-	public function __construct($file,$no_vendor,String $token,String $send_guildId,String $send_channelId,String $receive_channelId){
+	protected $D2P_Queue;
+	protected $P2D_Queue;
+
+	public function __construct($file, $no_vendor, string $token, string $send_guildId, string $send_channelId, string $receive_channelId){
 		$this->file = $file;
 		$this->no_vendor = $no_vendor;
 		$this->token = $token;
 		$this->send_guildId = $send_guildId;
 		$this->send_channelId = $send_channelId;
 		$this->receive_channelId = $receive_channelId;
-		
+
+		$this->D2P_Queue = new \Threaded;
+		$this->P2D_Queue = new \Threaded;
+
 		$this->start();
 	}
- 
+
 	public function run(){
 		if(!$this->no_vendor){
 			include $this->file."vendor/autoload.php";
@@ -44,18 +49,23 @@ class discordThread extends \Thread{
 			"loop" => $loop,
 		]);
 
+		//sleep(1);//...?
+
+		$timer = $loop->addPeriodicTimer(1, function() use ($discord){
+			if($this->stopped){
+				$discord->close();
+				$discord->loop->stop();
+				$this->started = false;
+				return;
+			}
+		});
+
+		$timer1 = $loop->addPeriodicTimer(1, function() use ($discord){
+			$this->task($discord);
+		});
+
+
 		unset($this->token);
-
-		$send_guildId = $this->send_guildId;
-		$send_channelId = $this->send_channelId;
-
-		$timer = $loop->addPeriodicTimer(2, function() use ($discord,$send_guildId,$send_channelId,$loop){
-			$this->sendtask($discord,$send_guildId,$send_channelId,$loop);
-		});
-
-		$timer = $loop->addPeriodicTimer(0.05, function() use ($discord,$send_guildId,$send_channelId,$loop){
-			$this->checktask($discord,$send_guildId,$send_channelId,$loop);
-		});
 
 		$discord->on('ready', function($discord){
 			$this->started = true;
@@ -63,97 +73,60 @@ class discordThread extends \Thread{
 			// Listen for events here
 			$botUserId = $discord->user->id;
 			$receive_channelId = $this->receive_channelId;
-			$discord->on('message', function($message) use ($discord,$botUserId,$receive_channelId){
-				//var_dump($message);
+
+			$discord->on('message', function($message) use ($botUserId, $receive_channelId){
 				if($message->channel->id === $receive_channelId){
-					//echo "Recieved a message from {$message->author->username}: {$message->content}", PHP_EOL;
 					if($message->author->user->id === $botUserId) return;
-					$content = $message->content;
-					//ヒント: 109行付近より同期スレッド間データー受け渡しを実施しております...
-					$this->synchronized(function() use ($content){
-						$this->content = $content;
-						$this->synchronized1 = true;
-						$this->wait();
-					});
+					$this->D2P_Queue[] = serialize([
+						'username' => $message->author->username,
+						'content' => $message->content
+					]);
 				}
 			});
 		});
 		$discord->run();
-		//var_dump("stop!!");
 	}
 
-	public function sendtask($discord,$send_guildId,$send_channelId,$loop){
+	public function task($discord){
 		if(!$this->started) return;
-		$test = preg_replace(['/\]0;.*\%/','/[\x07]/',"/Server thread\//"],'',TextFormat::clean(substr($this->test,0,1900)));//processtile,ANSIコードの削除を実施致します...
-		$this->test = strlen($this->test) <= 1900 ? "" : substr($this->test,1900);//
-		if($test !== ""){
-	        $guild = $discord->guilds->get('id', $send_guildId);
-			$channel = $guild->channels->get('id', $send_channelId);
-			$channel->sendMessage("```\n".$test."\n```");
-			$this->test = "";
+
+		$guild = $discord->guilds->get('id', $this->send_guildId);
+		$channel = $guild->channels->get('id', $this->send_channelId);
+
+		$send = "";
+
+		while(count($this->P2D_Queue) > 0){
+			$message = unserialize($this->P2D_Queue->shift());//
+			$message = preg_replace(['/\]0;.*\%/', '/[\x07]/', "/Server thread\//"], '', TextFormat::clean(substr($message, 0, 1900)));//processtile,ANSIコードの削除を実施致します...
+			if($message === "") continue;
+			$send .= $message;
+			if(strlen($send) >= 1800){
+				break;
+			}
 		}
-	}
-
-	public function checktask($discord,$send_guildId,$send_channelId,$loop){
-		if(!$this->started) return;
-		if($this->stopped){
-			$discord->close();
-			$loop->stop();
-			$this->started = false;
-			return;
+		if($send !== ""){
+			$channel->sendMessage("```".$send."```");
 		}
 
-		if(!$this->synchronized) return;
-
-		//ヒント: 132行付近より同期スレッド間データー受け渡しを実施しております...
-		$this->synchronized(function() use ($discord){
-			$this->synchronized = false;
-            $this->wait();
-        });
 	}
 
 	//===メインスレッド呼び出し専用関数にてございます...===
-
-	public function closeThread(){
-		$this->synchronized(function($thread){
-			$this->synchronized = true;
-			$thread->stopped = true;
-			$thread->notify();
-			//var_dump("closeThread");
-		}, $this);
+	public function shutdown(){
+		$this->stopped = true;
 	}
 
-	public function addText($text){
-		$this->synchronized = true;
-
-		//ヒント: 83行付近より同期スレッド間データー受け渡しを実施しております...
-		$this->synchronized(function($thread) use ($text){
-			//
-			//var_dump("addText: ".$text);
-			$thread->test .= $text;
-			$thread->notify();
-		}, $this);
+	public function sendMessage(String $message){
+		//var_dump("send".$message);
+		$this->P2D_Queue[] = serialize($message);
 	}
 
-	public function mainThreadTask(){
-		//ヒント: 56行付近より同期スレッド間データー受け渡しを実施しております...
-		if($this->synchronized1 === true){
-			$this->synchronized1 = false;
-			$this->synchronized(function($thread){
-				//var_dump($thread->content);
-				$content = $thread->content;
-				if($content[0] === "/"){
-					//var_dump("!!");
-					Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), substr($content, 1));
-				}else{
-					//var_dump("??");
-					Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), "me ".$content);
-				}
-				
-				$thread->content = "";
-				//var_dump($thread->content);
-				$thread->notify();
-			}, $this);
+	public function fetchMessages(){
+		//var_dump("?!?!");
+		$messages = [];
+		while(count($this->D2P_Queue) > 0){
+			$messages[] = unserialize($this->D2P_Queue->shift());
 		}
+		//var_dump($messages);
+		return $messages;
 	}
 }
--- src/pmmpDiscordBot/pmmpDiscordBot.php
+++ src/pmmpDiscordBot/pmmpDiscordBot.php
@@ -1,89 +1,93 @@
 <?php
+
 namespace pmmpDiscordBot;
 
-use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\utils\Config;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
-use pocketmine\scheduler\Task;
-use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
+use pocketmine\Server;
+use pocketmine\utils\Config;
 
 class pmmpDiscordBot extends PluginBase implements Listener{
-    public $client;
-    public $started = false;
+	/** @var discordThread */
+	public $client;
+	public $started = false;
 
-    public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info("discordbotをバックグラウンドにて起動しております...");
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info("discordbotをバックグラウンドにて起動しております...");
 
-        $this->saveResource("token.yml");
-        $this->saveResource("setting.yml");
-        $tokenConfig = new Config($this->getDataFolder()."token.yml",Config::YAML);
-        $settingConfig = new Config($this->getDataFolder()."setting.yml",Config::YAML);
+		$this->saveResource("token.yml");
+		$this->saveResource("setting.yml");
+		$tokenConfig = new Config($this->getDataFolder()."token.yml", Config::YAML);
+		$settingConfig = new Config($this->getDataFolder()."setting.yml", Config::YAML);
 
-        $error = false;
-        $token = $tokenConfig->get("token","your-auth-token");
-        $send_guildId = $tokenConfig->get("send_guildId","your-guild-id");
-        $send_channelId = $tokenConfig->get("send_channelId","your-channel-id");
-        $receive_channelId = $tokenConfig->get("receive_channelId","your-channel-id");
-        if($token === "your-auth-token"||$send_guildId === "your-guild-id"||$send_channelId === "your-channel-id"||$receive_channelId === "your-channel-id"){
-            $this->getLogger()->info("[PocketMine-MP]/plugin_data/pmmpDiscordBot/token.yml 上の一部の設定に関しましては不正の為、ファイルの中身を編集していただきたいです...");
-            $error = true;
-        }
+		$error = false;
+		$token = $tokenConfig->get("token", "your-auth-token");
+		$send_guildId = $tokenConfig->get("send_guildId", "your-guild-id");
+		$send_channelId = $tokenConfig->get("send_channelId", "your-channel-id");
+		$receive_channelId = $tokenConfig->get("receive_channelId", "your-channel-id");
+		if($token === "your-auth-token"||$send_guildId === "your-guild-id"||$send_channelId === "your-channel-id"||$receive_channelId === "your-channel-id"){
+			$this->getLogger()->info("[PocketMine-MP]/plugin_data/pmmpDiscordBot/token.yml 上の一部の設定に関しましては不正の為、ファイルの中身を編集していただきたいです...");
+			$error = true;
+		}
 
-        $no_vendor = $settingConfig->get("no-vendor");
-        if(!$no_vendor&&!file_exists($this->getFile()."vendor/autoload.php")){
-            $this->getLogger()->error($this->getFile()."vendor/autoload.php ファイルに関しましては存在致しません為、discordbotを起動することは出来ません。");
-            $this->getLogger()->info("§ehttps://github.com/DaisukeDaisuke/pmmpDiscordBot/releases よりphar形式のプラグインをダウンロードお願い致します。§r");
-            $error = true;
-        }
-        
-        if($error === true){
-            $this->getLogger()->info("§cこのプラグインを無効化致します。§r");
-            $this->getServer()->getPluginManager()->disablePlugin($this);
-            return;
-        }
+		$no_vendor = $settingConfig->get("no-vendor");
+		if(!$no_vendor&&!file_exists($this->getFile()."vendor/autoload.php")){
+			$this->getLogger()->error($this->getFile()."vendor/autoload.php ファイルに関しましては存在致しません為、discordbotを起動することは出来ません。");
+			$this->getLogger()->info("§ehttps://github.com/DaisukeDaisuke/pmmpDiscordBot/releases よりphar形式のプラグインをダウンロードお願い致します。§r");
+			$error = true;
+		}
 
-        $this->client = new discordThread($this->getFile(),$no_vendor,$token,$send_guildId,$send_channelId,$receive_channelId);
+		if($error === true){
+			$this->getLogger()->info("§cこのプラグインを無効化致します。§r");
+			$this->getServer()->getPluginManager()->disablePlugin($this);
+			return;
+		}
 
-        $this->getScheduler()->scheduleDelayedTask(new ClosureTask(
-            function (int $currentTick): void {
-                $this->started = true;
-                $this->getLogger()->info("出力バッファリングを開始致します。");
-                ob_start();
-            }
-        ),10);
+		$this->client = new discordThread($this->getFile(), $no_vendor, $token, $send_guildId, $send_channelId, $receive_channelId);
 
-        $this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(
-            function (int $currentTick): void{
-                if(!$this->started) return;
-                $string = ob_get_contents();
-                //file_put_contents($this->getDataFolder()."/test.txt",preg_replace('/\]0;.*\%/','',TextFormat::clean($string)),FILE_APPEND);
-                //var_dump(preg_replace('#\\x1b[[][^A-Za-z]*[A-Za-z]#', '',preg_replace('/[\x1b|\x07].*[\x1b|\x07]/', '', $string)));
-                //var_dump(preg_replace(['/\]0;.*\%/','/[\x07]/'],'',TextFormat::clean($string)));
-                $this->client->addText($string);
-                //echo "!";
-                ob_flush();
-            }
-        ),10,1);
-        
-        $this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(
-            function(int $currentTick): void{
-                $string = ob_get_contents();
-                $this->client->mainThreadTask();//非同期スレッドよりコマンドを受信、コマンドを実行致します。
-            }
-        ), 10, 2);
-    }
+		$this->getScheduler()->scheduleDelayedTask(new ClosureTask(
+			function(int $currentTick): void{
+				$this->started = true;
+				$this->getLogger()->info("出力バッファリングを開始致します。");
+				ob_start();
+			}
+		), 10);
 
-    public function onDisable(){
-        if(!$this->started) return;
-        $this->getLogger()->info("出力バッファリングを終了しています...");
-        $this->client->closeThread();
-        ob_flush();
-        ob_end_clean();
-        $this->getLogger()->info("discordBotの終了を待機しております...");
-        $this->client->join();
-    }
+		$this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(
+			function(int $currentTick): void{
+				if(!$this->started) return;
+				$string = ob_get_contents();
+
+				if($string === "") return;
+				$this->client->sendMessage($string);
+				ob_flush();
+			}
+		), 10, 1);
+
+		$this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(
+			function(int $currentTick): void{
+				foreach($this->client->fetchMessages() as $message){
+					$content = $message["content"];
+					if($content[0] === "/"){
+						Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), substr($content, 1));
+					}else{
+						Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), "me ".$content);
+					}
+				}
+			}
+		), 5, 2);
+	}
+
+	public function onDisable(){
+		if(!$this->started) return;
+		$this->getLogger()->info("出力バッファリングを終了しています...");
+		$this->client->shutdown();
+		ob_flush();
+		ob_end_clean();
+		$this->getLogger()->info("discordBotの終了を待機しております...");
+		$this->client->join();
+	}
 }
