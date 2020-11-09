3da548ac3621973a8ad17076b0b0e622237bb01c
DaisukeDaisuke <akblcmakblcm@gmail.com>
2020-11-09 09:04:16 +0900

    update

--- src/pmmpDiscordBot/discordThread.php
+++ src/pmmpDiscordBot/discordThread.php
@@ -2,9 +2,32 @@
 
 namespace pmmpDiscordBot;
 
+use Discord\Discord;
+use Discord\Parts\Channel\Channel;
+use Discord\Parts\User\Member;
+use Discord\WebSockets\Event;
 use pocketmine\utils\TextFormat;
 
 class discordThread extends \Thread{
+	const MESSAGE_TYPE = 0;//int
+	const MESSAGE = 1;//string
+	const MESSAGE_ID = 2;//string
+	const MESSAGE_GUILDID = 3;//string
+	const MESSAGE_CHANNELID = 4;//string
+	const MESSAGE_USERNAME = 5;//string
+	const MESSAGE_USERID = 6;//string
+	const MESSAGE_ISBOT = 7;//bool
+	const MESSAGE_IS_MYSELF = 8;//bool
+	const MESSAGE_IS_DM = 9;//bool
+
+	const MESSAGE_EMBEDS = 10;
+
+	const MESSAGE_EMOJI = 11;//絵文字本体「🤔」等...
+	const MESSAGE_EMOJI_ID = 12;//????? 絵文字Id「null」等...
+	const MESSAGE_DISCRIMINATOR = 13;
+	//const MESSAGE = 1;
+	//const MESSAGE_ID = 2;
+
 	public $file;
 
 	public $stopped = false;
@@ -14,20 +37,27 @@ class discordThread extends \Thread{
 	private $token;
 	public $send_guildId;
 	public $send_channelId;
-	public $receive_channelId;
 	public $send_interval;
 	public $receive_check_interval;
 
 	protected $D2P_Queue;
 	protected $P2D_Queue;
 
-	public function __construct($file, $no_vendor, string $token, string $send_guildId, string $send_channelId, string $receive_channelId, int $send_interval = 1){
+	const MESSAGE_TYPE_REPLY = -1;
+	const MESSAGE_TYPE_SEND = 0;
+	const MESSAGE_TYPE_EDIT = 1;
+	const MESSAGE_TYPE_EMOJI_ADD = 2;
+	const MESSAGE_TYPE_EMOJI_REMOVE = 3;
+	const MESSAGE_TYPE_MEMBER_ADD = 4;
+	const MESSAGE_TYPE_MEMBER_REMOVE = 5;
+	const MESSAGE_TYPE_DELETE = 6;
+
+	public function __construct($file, $no_vendor, string $token, string $send_guildId, string $send_channelId, int $send_interval = 1){
 		$this->file = $file;
 		$this->no_vendor = $no_vendor;
 		$this->token = $token;
 		$this->send_guildId = $send_guildId;
 		$this->send_channelId = $send_channelId;
-		$this->receive_channelId = $receive_channelId;
 
 		$this->send_interval = $send_interval;
 
@@ -52,61 +82,216 @@ class discordThread extends \Thread{
 
 		//sleep(1);//...?
 
-		$timer = $loop->addPeriodicTimer(1, function() use ($discord){
-			if($this->stopped){
-				$discord->close();
-				$discord->loop->stop();
-				$this->started = false;
-				return;
-			}
-		});
-
-		$timer1 = $loop->addPeriodicTimer(1, function() use ($discord){
-			$this->task($discord);
-		});
-
-
 		unset($this->token);
 
-		$discord->on('ready', function($discord){
+		$discord->on('ready', function(\Discord\Discord $discord) use ($loop){
 			$this->started = true;
 			echo "Bot is ready.", PHP_EOL;
+
+			$guild = $discord->guilds->get('id', $this->send_guildId);
+			//$channel = $guild->channels->get('id', $this->send_channelId);
+
+			//var_dump("!!");
+			/*var_dump($guild->emojis->save($guild)->then(function ($test){
+				$test->freshen();
+			}));*/
+			//var_dump("!!");
+
+			$channel = $discord->factory(Channel::class, ['id' => $this->send_channelId]);
+
+			$timer = $loop->addPeriodicTimer(1, function() use ($discord){
+				if($this->stopped){
+					$discord->close();
+					$discord->loop->stop();
+					$this->started = false;
+					return;
+				}
+			});
+
+			$timer1 = $loop->addPeriodicTimer(1, function() use ($discord, $channel){
+				$this->task($discord, $channel);
+			});
+
 			// Listen for events here
 			$botUserId = $discord->user->id;
-			$receive_channelId = $this->receive_channelId;
+			// = $this->receive_channelId;
 
-			$discord->on('message', function($message) use ($botUserId, $receive_channelId){
-				if($message->channel->id === $receive_channelId){
-					if($message->author->user->id === $botUserId) return;
+			$discord->on('message', function(\Discord\Parts\Channel\Message $message) use ($botUserId){//, $receive_channelId
+				//$message->react("🤔");
+				if($message->author instanceof Member){
 					$this->D2P_Queue[] = serialize([
-						'username' => $message->author->username,
-						'content' => $message->content
+						self::MESSAGE_TYPE => self::MESSAGE_TYPE_REPLY,//
+						self::MESSAGE => $message->content,
+						self::MESSAGE_ID => $message->id,
+						self::MESSAGE_GUILDID => $message->channel->guild_id,
+						self::MESSAGE_CHANNELID => $message->channel->id,
+						self::MESSAGE_USERNAME => $message->author->username,
+						self::MESSAGE_USERID => $message->author->id,
+						self::MESSAGE_ISBOT => $message->author->user->bot ?? false,
+						self::MESSAGE_IS_MYSELF => ($message->author->user->id === $botUserId),
+						self::MESSAGE_IS_DM => false,
+						//'md5' => md5($message["content"]),
+					]);
+				}else{
+					$this->D2P_Queue[] = serialize([
+						self::MESSAGE_TYPE => self::MESSAGE_TYPE_REPLY,//
+						self::MESSAGE => $message->content,
+						self::MESSAGE_ID => $message->id,
+						self::MESSAGE_GUILDID => $message->channel->guild_id,
+						self::MESSAGE_CHANNELID => $message->channel->id,
+						self::MESSAGE_USERNAME => $message->author->username,
+						self::MESSAGE_USERID => $message->author->id,
+						self::MESSAGE_ISBOT => $message->author->bot ?? false,
+						self::MESSAGE_IS_MYSELF => ($message->author->id === $botUserId),
+						self::MESSAGE_IS_DM => true,
+						//'md5' => md5($message["content"]),
 					]);
 				}
+			});
+			$discord->on("MESSAGE_REACTION_ADD", function(\stdClass $obj, Discord $discord) use ($botUserId){
+				if(isset($obj->member)){
+					$this->D2P_Queue[] = serialize([
+						self::MESSAGE_TYPE => self::MESSAGE_TYPE_EMOJI_ADD,//
+
+						self::MESSAGE_ID => $obj->message_id,
+						self::MESSAGE_GUILDID => $obj->guild_id,
+						self::MESSAGE_CHANNELID => $obj->channel_id,
+						self::MESSAGE_USERNAME => $obj->member->user->username,
+						self::MESSAGE_USERID => $obj->user_id,
+
+						self::MESSAGE_IS_MYSELF => ($obj->user_id === $botUserId),
+						self::MESSAGE_IS_DM => false,
+
+						self::MESSAGE_EMOJI => $obj->emoji->name,
+						self::MESSAGE_EMOJI_ID => $obj->emoji->id,
+
+						//'md5' => md5($message["content"]),
+					]);
+				}else{
+					$this->D2P_Queue[] = serialize([
+						self::MESSAGE_TYPE => self::MESSAGE_TYPE_EMOJI_ADD,//
+
+						self::MESSAGE_ID => $obj->message_id,
+						self::MESSAGE_USERID => $obj->user_id,
+
+						self::MESSAGE_IS_MYSELF => ($obj->user_id === $botUserId),
+						self::MESSAGE_IS_DM => true,
+
+						self::MESSAGE_EMOJI => $obj->emoji->name,
+						self::MESSAGE_EMOJI_ID => $obj->emoji->id,
+					]);
+
+				}
+			});
+			$discord->on("MESSAGE_REACTION_REMOVE",function(\stdClass $obj, Discord $discord) use ($botUserId){
+				if(isset($obj->guild_id)){
+					$this->D2P_Queue[] = serialize([
+						self::MESSAGE_TYPE => self::MESSAGE_TYPE_EMOJI_REMOVE,//
+
+						self::MESSAGE_ID => $obj->message_id,
+						self::MESSAGE_GUILDID => $obj->guild_id,
+						self::MESSAGE_CHANNELID => $obj->channel_id,
+						self::MESSAGE_USERID => $obj->user_id,
+
+						self::MESSAGE_IS_MYSELF => ($obj->user_id === $botUserId),
+						self::MESSAGE_IS_DM => false,
+
+						self::MESSAGE_EMOJI => $obj->emoji->name,
+						self::MESSAGE_EMOJI_ID => $obj->emoji->id,
+
+						//'md5' => md5($message["content"]),
+					]);
+				}else{
+					$this->D2P_Queue[] = serialize([
+						self::MESSAGE_TYPE => self::MESSAGE_TYPE_EMOJI_REMOVE,//
+
+						self::MESSAGE_ID => $obj->message_id,
+						self::MESSAGE_CHANNELID => $obj->channel_id,
+						self::MESSAGE_USERID => $obj->user_id,
+
+						self::MESSAGE_IS_MYSELF => ($obj->user_id === $botUserId),
+						self::MESSAGE_IS_DM => true,
+
+						self::MESSAGE_EMOJI => $obj->emoji->name,
+						self::MESSAGE_EMOJI_ID => $obj->emoji->id,
+					]);
+				}
+			});
+			$discord->on("GUILD_MEMBER_ADD",function(Member $member,Discord $discord) use ($botUserId){
+				$this->D2P_Queue[] = serialize([
+					self::MESSAGE_TYPE => self::MESSAGE_TYPE_MEMBER_ADD,//
+
+					self::MESSAGE_GUILDID => $member->guild_id,
+					self::MESSAGE_USERNAME => $member->user->username,
+					self::MESSAGE_USERID => $member->user->id,
+
+					self::MESSAGE_ISBOT => $member->user->bot ?? false,
+					self::MESSAGE_IS_MYSELF => ($member->user->id === $botUserId),
+
+					self::MESSAGE_DISCRIMINATOR => $member->user->discriminator,
+				]);
+			});
+			$discord->on("GUILD_MEMBER_REMOVE",function(Member $member,Discord $discord) use ($botUserId){
+				$this->D2P_Queue[] = serialize([
+					self::MESSAGE_TYPE => self::MESSAGE_TYPE_MEMBER_REMOVE,//
+
+					self::MESSAGE_GUILDID => $member->guild_id,
+					self::MESSAGE_USERNAME => $member->user->username,
+					self::MESSAGE_USERID => $member->user->id,
+
+					self::MESSAGE_ISBOT => $member->user->bot ?? false,
+					self::MESSAGE_IS_MYSELF => ($member->user->id === $botUserId),
+
+					self::MESSAGE_DISCRIMINATOR => $member->user->discriminator,
+					//joined_at...?
+				]);
+			});
+			$discord->on("MESSAGE_DELETE",function(\stdClass $obj, Discord $discord) use ($botUserId){
+				$this->D2P_Queue[] = serialize([
+					self::MESSAGE_TYPE => self::MESSAGE_TYPE_DELETE,//
+
+					self::MESSAGE_ID => $obj->id,
+					self::MESSAGE_CHANNELID => $obj->channel_id,
+					self::MESSAGE_GUILDID => $obj->guild_id,
+				]);
 			});
 		});
 		$discord->run();
 	}
 
-	public function task($discord){
+	public function messageUpdate($discord, string $messageId, $channel, string $contents){
+		$channel = $channel instanceof Channel ? $channel : $discord->factory(Channel::class, ['id' => $channel]);
+		$channel->messages->fetch($messageId)->then(function($message) use ($channel, $contents){
+			$message->content = $contents;
+
+			$channel->messages->save($message)->then(function($message) use ($channel){
+				// message has been updated
+			});
+		});
+	}
+
+	public function task($discord, $channel){
 		if(!$this->started) return;
-
-		$guild = $discord->guilds->get('id', $this->send_guildId);
-		$channel = $guild->channels->get('id', $this->send_channelId);
-
 		$send = "";
 
 		while(count($this->P2D_Queue) > 0){
-			$message = unserialize($this->P2D_Queue->shift());//
-			$message = preg_replace(['/\]0;.*\%/', '/[\x07]/', "/Server thread\//"], '', TextFormat::clean(substr($message, 0, 1900)));//processtile,ANSIコードの削除を実施致します...
-			if($message === "") continue;
-			$send .= $message;
-			if(strlen($send) >= 1800){
-				break;
+			$message = unserialize($this->P2D_Queue->shift());
+			switch($message[self::MESSAGE_TYPE]){
+				case self::MESSAGE_TYPE_SEND:
+					$channel = $message[self::MESSAGE_CHANNELID] === null ? $channel : $discord->factory(Channel::class, ['id' => $message[self::MESSAGE_CHANNELID]]);
+					$channelId = $channel->id;
+					$send = preg_replace(['/\]0;.*\%/', '/[\x07]/', "/Server thread\//"], '', TextFormat::clean(substr($message[self::MESSAGE], 0, 1900)));//processtile,ANSIコードの削除を実施致します...
+					if($send === "") continue 2;
+					//$send .= $message;//."\n";
+					//if(strlen($send) >= 1800){
+					$channel->sendMessage($send, false, $message[self::MESSAGE_EMBEDS] ?? null);//message, tts message, embeds message
+					break;
+				case self::MESSAGE_TYPE_EDIT:
+					$this->messageUpdate($discord, $message[self::MESSAGE_ID], $message[self::MESSAGE_CHANNELID] ?? $channel, $message[self::MESSAGE]);
+					break;
 			}
-		}
-		if($send !== ""){
-			$channel->sendMessage("```".$send."```");
+			//$message = unserialize($this->P2D_Queue->shift());//
+
 		}
 
 	}
@@ -116,9 +301,28 @@ class discordThread extends \Thread{
 		$this->stopped = true;
 	}
 
-	public function sendMessage(string $message){
+	public function replyMessage(string $userId, ?string $channelId, string $message, ?array $embeds = null){
+		$this->sendMessage("<@".$userId.">, ".$message, $channelId, $embeds);
+	}
+
+	public function sendMessage(string $message, ?string $channelId = null, ?array $embeds = null){
 		//var_dump("send".$message);
-		$this->P2D_Queue[] = serialize($message);
+		$this->P2D_Queue[] = serialize([
+			self::MESSAGE_TYPE => self::MESSAGE_TYPE_SEND,
+			self::MESSAGE => $message,
+			self::MESSAGE_CHANNELID => $channelId,
+			self::MESSAGE_EMBEDS => $embeds,
+		]);
+	}
+
+	public function editMessage(string $messageId, ?string $channelId = null, string $message){
+		$this->P2D_Queue[] = serialize([
+			self::MESSAGE_TYPE => self::MESSAGE_TYPE_EDIT,
+			self::MESSAGE => $message,
+			self::MESSAGE_ID => $messageId,
+			self::MESSAGE_CHANNELID => $channelId,
+			//self::MESSAGE_EMBEDS => $embeds,
+		]);
 	}
 
 	public function fetchMessages(){
--- src/pmmpDiscordBot/pmmpDiscordBot.php
+++ src/pmmpDiscordBot/pmmpDiscordBot.php
@@ -2,11 +2,9 @@
 
 namespace pmmpDiscordBot;
 
-use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
-use pocketmine\Server;
 use pocketmine\utils\Config;
 
 class pmmpDiscordBot extends PluginBase implements Listener{
@@ -15,6 +13,8 @@ class pmmpDiscordBot extends PluginBase implements Listener{
 	public $started = false;
 
 	public $receive_check_interval;
+
+	public $messageids = [];
 
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -29,11 +29,11 @@ class pmmpDiscordBot extends PluginBase implements Listener{
 		$token = $tokenConfig->get("token", "your-auth-token");
 		$send_guildId = $tokenConfig->get("send_guildId", "your-guild-id");
 		$send_channelId = $tokenConfig->get("send_channelId", "your-channel-id");
-		$receive_channelId = $tokenConfig->get("receive_channelId", "your-channel-id");
+		//$receive_channelId = $tokenConfig->get("receive_channelId", "your-channel-id");
 
 		unset($tokenConfig);
 
-		if($token === "your-auth-token"||$send_guildId === "your-guild-id"||$send_channelId === "your-channel-id"||$receive_channelId === "your-channel-id"){
+		if($token === "your-auth-token"||$send_guildId === "your-guild-id"||$send_channelId === "your-channel-id"){
 			$this->getLogger()->info("[PocketMine-MP]/plugin_data/pmmpDiscordBot/token.yml 上の一部の設定に関しましては不正の為、ファイルの中身を編集していただきたいです...");
 			$error = true;
 		}
@@ -59,11 +59,12 @@ class pmmpDiscordBot extends PluginBase implements Listener{
 			return;
 		}
 
-		$this->client = new discordThread($this->getFile(), $no_vendor, $token, $send_guildId, $send_channelId, $receive_channelId, $send_interval);
+		$this->client = new discordThread($this->getFile(), $no_vendor, $token, $send_guildId, $send_channelId, $send_interval);
+		$this->started = true;
 
 		unset($token);
 
-		$this->getScheduler()->scheduleDelayedTask(new ClosureTask(
+		/*$this->getScheduler()->scheduleDelayedTask(new ClosureTask(
 			function(int $currentTick): void{
 				$this->started = true;
 				$this->getLogger()->info("出力バッファリングを開始致します。");
@@ -80,28 +81,114 @@ class pmmpDiscordBot extends PluginBase implements Listener{
 				$this->client->sendMessage($string);
 				ob_flush();
 			}
-		), 10, 1);
+		), 10, 1);*/
 
 		$this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(
 			function(int $currentTick): void{
 				foreach($this->client->fetchMessages() as $message){
-					$content = $message["content"];
-					if($content[0] === "/"){
-						Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), substr($content, 1));
-					}else{
-						Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), "me ".$content);
+					switch($message[discordThread::MESSAGE_TYPE]){
+						case discordThread::MESSAGE_TYPE_REPLY:
+							/*if($message->channel->id !== $receive_channelId){
+								return;
+							}*/
+							/** @var mixed[] $message */
+							if($message[discordThread::MESSAGE_IS_MYSELF]){
+								$this->messageids[] = [$message[discordThread::MESSAGE_ID], $message[discordThread::MESSAGE_CHANNELID]];//
+								//$this->client->editMessage($message[discordThread::MESSAGE_ID], $message[discordThread::MESSAGE_CHANNELID],$contents);
+								//var_dump("!!!!!!");
+								//$this->client->editMessage($message[discordThread::MESSAGE_ID], null,"test!!!!!!!!!");
+								//$this->editMessage($message, "test!!!!!!!!");
+
+								return;
+							}
+							if($message[discordThread::MESSAGE_ISBOT]){
+								return;
+							}
+							$content = $message[discordThread::MESSAGE];
+
+							//var_dump($content);
+
+							//$this->replyMessage($message,"test!!!!!");
+							/*$this->replyMessage($message, "test!!",
+								[
+									"description" => "▸全てのサービスは正常に稼働しています。 \n▸All Service are working!\n ",
+									"color" => 65280,
+									"author" => [
+										"name" => "Server Status",
+									],
+									"fields" => [
+										[
+											"name" => ":white_check_mark:Online",
+											"value" => "- Players: 0/10\n- Game: 待機中"
+										],
+									]
+								]
+							);*/
+
+							/*$this->sendMessage("test!!", null,
+								[
+									"description" => "▸全てのサービスは正常に稼働しています。 \n▸All Service are working!\n ",
+									"color" => 65280,
+									"author" => [
+										"name" => "Server Status",
+									],
+									"fields" => [
+										[
+											"name" => ":white_check_mark:Online",
+											"value" => "- Players: 0/10\n- Game: 待機中"
+										],
+									]
+								]
+							);*/
+
+							//receive message
+
+							/*if($content[0] === "/"){
+								Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), substr($content, 1));
+							}else{
+								Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), "me ".$content);
+							}*/
+							break;
+						case discordThread::MESSAGE_TYPE_EMOJI_ADD:
+							//var_dump($message);
+							break;
+						case discordThread::MESSAGE_TYPE_EMOJI_REMOVE:
+							//var_dump($message);
+							break;
+						case discordThread::MESSAGE_TYPE_MEMBER_ADD:
+							//var_dump("MESSAGE_TYPE_MEMBER_ADD",$message,"MESSAGE_TYPE_MEMBER_ADD");
+							break;
+						case discordThread::MESSAGE_TYPE_MEMBER_REMOVE:
+							//var_dump("MESSAGE_TYPE_MEMBER_REMOVE",$message,"MESSAGE_TYPE_MEMBER_REMOVE");
+							break;
+						case discordThread::MESSAGE_TYPE_DELETE:
+							//var_dump($message);
+							break;
 					}
 				}
 			}
 		), 5, $this->receive_check_interval);
+
+	}
+
+	public function editMessage(array $message, string $contents){
+		$this->client->editMessage($message[discordThread::MESSAGE_ID], $message[discordThread::MESSAGE_CHANNELID], $contents);
+	}
+
+	public function replyMessage(array $message, string $contents, ?array $embeds = null){
+		$this->client->replyMessage($message[discordThread::MESSAGE_USERID], $message[discordThread::MESSAGE_CHANNELID], $contents, $embeds);
+	}
+
+	public function sendMessage(string $contents, ?string $channelId = null, ?array $embeds = null){
+		$this->client->sendMessage($contents, $channelId, $embeds);
 	}
 
 	public function onDisable(){
 		if(!$this->started) return;
-		$this->getLogger()->info("出力バッファリングを終了しています...");
+		//$this->getLogger()->info("出力バッファリングを終了しています...");
 		$this->client->shutdown();
-		ob_flush();
-		ob_end_clean();
+		//ob_flush();
+		//ob_end_clean();
 		$this->getLogger()->info("discordBotの終了を待機しております...");
 		$this->client->join();
 	}
